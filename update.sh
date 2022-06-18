8a256e8c9f2a13a4af2ff2a187a279065d29a094
Dima Povarchuk <povarchukdima007@gmail.com>
2022-06-18 11:41:53 +0300

    New changes!

--- update.sh
+++ update.sh
@@ -3,5 +3,8 @@ cd /home/ubuntu/blitz-tactics
 sudo docker-compose stop
 sudo mkdir /home/ubuntu/demo3
 sudo git clone https://github.com/dimapovarchuk/blitz-tactics.git /home/ubuntu/demo3
-cd /home/ubuntu/demo3/
+cd /home/ubuntu/demo3
+sudo cp -rf * /home/ubuntu/blitz-tactics
+sudo rm /home/ubuntu/blitz-tactics/Gemfile.lock
+cd /home/ubuntu/blitz-tactics
 sudo docker-compose up -d
