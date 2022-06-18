56d3739d4e3816f5f27736eb841ed76528ff8320
Dima Povarchuk <povarchukdima007@gmail.com>
2022-06-18 11:08:51 +0300

    New changes!

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /home/ubuntu/blitz-tactics
+sudo docker-compose stop
 sudo mkdir /home/ubuntu/demo3
 sudo git clone https://github.com/dimapovarchuk/blitz-tactics.git /home/ubuntu/demo3
 cd /home/ubuntu/demo3/
